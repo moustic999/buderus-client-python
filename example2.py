56f61df02894294742f1f2fc4cb998242ea7a123
moustic999 <moustic999@msn.com>
2018-11-14 20:29:48 +0100

    tt

--- example2.py
+++ example2.py
@@ -1,16 +1,14 @@
 import asyncio
-import sys
 
 import aiohttp
 
 from buderus.gateway import Gateway
 
-# access_key = 'pAtgNvhjdJ2LFJS2'
-# password = 'isnbtm0'
+
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        gateway =  Gateway(session, '192.168.1.8','pAtgNvhjdJ2LFJS2','isnbtm0')
+        gateway =  Gateway(session, '192.168.1.8','scrambled','scrambled')
         result = await gateway.get_json('/heatingCircuits/hc1/operationMode')
       
         value = result['value']
