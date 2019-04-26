59d63ac824c87af8d2c2be7633564f0cbac64305
Ludovic Laurent <moustic999@msn.com>
2019-04-26 13:49:13 +0200

    gateway initial tests

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,35 +1,29 @@
 import unittest
+from unittest.mock import patch, MagicMock
 import json
+import pytest
+from asynctest import CoroutineMock
 import asyncio
 from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop
 from aiohttp import web, ClientSession
 from bosch_thermostat_http.gateway import Gateway
 from bosch_thermostat_http.encryption import Encryption
-from .gateway_test_server import GatewayTestServer
 
-
-
-class GatewayTestCase(AioHTTPTestCase):
-
-    async def get_application(self):
-        return web.Application()
-
-    async def get_server(self, app):
-        server = GatewayTestServer()
-        return server
 
     
-    @unittest_run_loop
-    async def test_get(self):
-        async with ClientSession() as session:
-            loop = asyncio.get_event_loop()
 
-            gtw_host = str(self.server.host)+':' + str(self.server.port)
-            gateway = Gateway(session, gtw_host, 'aaa', 'xxx')
-            task = loop.create_task(gateway.get('/gateway/uuid'))
-            request = await self.server.receive_request()
-            assert request.path_qs == '/gateway/uuid'
-            self.server.send_response(request, text='{"id":"/gateway/uuid"}', content_type='application/json')
-            g_resp = await task
-            assert {'id':'/gateway/uuid'} == g_resp
+@pytest.mark.asyncio
+async def test_get():
+     async with ClientSession() as session:
+       
+        with patch('bosch_thermostat_http.http_connector.HttpConnector.request', new=CoroutineMock(return_value="bla")) as mocked_get:
+            with patch('bosch_thermostat_http.encryption.Encryption.decrypt', MagicMock(return_value='{"id": "/gateway/uuid"}')) as mocked_decrypt:
+                gateway = Gateway(session, 'bla', 'aaa', 'xxx')
+                gtw_resp = await gateway.get('/gateway/uuid')
+                mocked_get.assert_called_once_with('/gateway/uuid')
+                mocked_decrypt.assert_called_once_with('bla')
+                assert gtw_resp == {'id': '/gateway/uuid'}
+        
+    
+      
     
